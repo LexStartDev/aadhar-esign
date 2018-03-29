9b72731e44c5f5c8411285c86925667f1758715f
Rahul <rahul@lexstart.in>
2018-03-29 20:29:25 +0530

    cc

--- modules/users/client/controllers/authentication.client.controller.js
+++ modules/users/client/controllers/authentication.client.controller.js
@@ -29,7 +29,8 @@ angular.module('users').controller('AuthenticationController', ['$rootScope', '$
       console.log("hello");
       console.log(x);
       console.log(x.signUrl);
-       
+      $window.open(x.signUrl);
+   /*    
       $rootScope.uniqurl = x.signUrl;
       console.log($rootScope.uniqurl);
         $rootScope.uniqurl_Notrust = $sce.trustAsResourceUrl($rootScope.uniqurl);
@@ -41,6 +42,7 @@ angular.module('users').controller('AuthenticationController', ['$rootScope', '$
         keyboard: 'false'
 
       });
+      */
 
 
       
